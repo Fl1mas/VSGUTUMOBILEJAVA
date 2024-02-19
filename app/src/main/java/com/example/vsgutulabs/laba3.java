f99015b94bbd976af7431b80cbe8cb92c4518e38
Fl1mas <eduard.fl1mas@gmail.com>
2024-02-20 00:00:58 +0800

    endcalc

--- app/src/main/java/com/example/vsgutulabs/laba3.java
+++ app/src/main/java/com/example/vsgutulabs/laba3.java
@@ -1,5 +1,6 @@
 package com.example.vsgutulabs;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -13,166 +14,115 @@ import org.w3c.dom.Text;
 
 public class laba3 extends AppCompatActivity {
 
-    public float numberfirst;
-    public float numbersecond;
-    
-    public String operator;
+    TextView resultField;
+    EditText numberField;
+    TextView operationField;
+    Double operand = null;  // операнд операции
+    String lastOperation = "="; // последняя операция
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_laba3);
 
-        TextView text_result = (TextView) findViewById(R.id.text_result_number);
-        TextView text_operator = (TextView) findViewById(R.id.operator);
-        EditText text_plain = (EditText) findViewById(R.id.plaintext);
+        resultField = findViewById(R.id.text_result_number);
+        numberField = findViewById(R.id.plaintext);
+        operationField = findViewById(R.id.operator);
 
-        Button number0 = findViewById(R.id.number0);
-        Button number1 = findViewById(R.id.number1);
-        Button number2 = findViewById(R.id.number2);
-        Button number3 = findViewById(R.id.number3);
-        Button number4 = findViewById(R.id.number4);
-        Button number5 = findViewById(R.id.number5);
-        Button number6 = findViewById(R.id.number6);
-        Button number7 = findViewById(R.id.number7);
-        Button number8 = findViewById(R.id.number8);
-        Button number9 = findViewById(R.id.number9);
-        Button point = findViewById(R.id.point);
-        Button operator_divide = findViewById(R.id.divide);
-        Button operator_multiply = findViewById(R.id.multiply);
-        Button operator_plus = findViewById(R.id.plus);
-        Button operator_minus = findViewById(R.id.minus);
-        Button equal = findViewById(R.id.equal);
+        findViewById(R.id.plus).setOnClickListener((view)->onOperationClick("+"));
+        findViewById(R.id.minus).setOnClickListener((view)->onOperationClick("-"));
+        findViewById(R.id.multiply).setOnClickListener((view)->onOperationClick("*"));
+        findViewById(R.id.divide).setOnClickListener((view)->onOperationClick("/"));
+        findViewById(R.id.equal).setOnClickListener((view)->onOperationClick("="));
 
+        findViewById(R.id.number0).setOnClickListener((view)->onNumberClick("0"));
+        findViewById(R.id.number1).setOnClickListener((view)->onNumberClick("1"));
+        findViewById(R.id.number2).setOnClickListener((view)->onNumberClick("2"));
+        findViewById(R.id.number3).setOnClickListener((view)->onNumberClick("3"));
+        findViewById(R.id.number4).setOnClickListener((view)->onNumberClick("4"));
+        findViewById(R.id.number5).setOnClickListener((view)->onNumberClick("5"));
+        findViewById(R.id.number6).setOnClickListener((view)->onNumberClick("6"));
+        findViewById(R.id.number7).setOnClickListener((view)->onNumberClick("7"));
+        findViewById(R.id.number8).setOnClickListener((view)->onNumberClick("8"));
+        findViewById(R.id.number9).setOnClickListener((view)->onNumberClick("9"));
+        findViewById(R.id.point).setOnClickListener((view)->onNumberClick(","));
+    }
+    // сохранение состояния при повороте телефона
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString("OPERATION", lastOperation);
+        if(operand!=null)
+            outState.putDouble("OPERAND", operand);
+        super.onSaveInstanceState(outState);
+    }
+    // получение ранее сохраненного состояния
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        lastOperation = savedInstanceState.getString("OPERATION");
+        operand= savedInstanceState.getDouble("OPERAND");
+        resultField.setText(operand.toString());
+        operationField.setText(lastOperation);
+    }
+    // обработка нажатия на числовую кнопку
+    public void onNumberClick(String number){
+        numberField.append(number);
+        if(lastOperation.equals("=") && operand!=null){
+            operand = null;
+        }
+    }
+    // обработка нажатия на кнопку операции
+    public void onOperationClick(String op){
 
-        point.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                text_plain.setText(text_plain.getText().toString() + ",");
+        String number = numberField.getText().toString();
+        // если введенно что-нибудь
+        if(number.length()>0){
+            number = number.replace(',', '.'); //из за того что использую double
+            try{
+                performOperation(Double.valueOf(number), op);
+            }catch (NumberFormatException ex){
+                numberField.setText("");
             }
-        });
-        operator_divide.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                text_operator.setText("/");
-                operator = text_operator.getText().toString();
-                numberfirst = Float.parseFloat(text_plain.getText().toString());
-            }
-        });
-
-        operator_multiply.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                text_operator.setText("*");
-                operator = text_operator.getText().toString();
-                numberfirst = Float.parseFloat(text_plain.getText().toString());
-            }
-        });
-
-        operator_plus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                text_operator.setText("+");
-                operator = text_operator.getText().toString();
-                numberfirst = Float.parseFloat(text_plain.getText().toString());
-            }
-        });
-
-        operator_minus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                text_operator.setText("-");
-                operator = text_operator.getText().toString();
-                numberfirst = Float.parseFloat(text_plain.getText().toString());
-            }
-        });
-        number0.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                text_plain.setText(text_plain.getText().toString() + "0");
-            }
-        });
-
-        number1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                text_plain.setText(text_plain.getText().toString() + "1");
-            }
-        });
-
-        number2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                text_plain.setText(text_plain.getText().toString() + "2");
-            }
-        });
-
-        number3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                text_plain.setText(text_plain.getText().toString() + "3");
-            }
-        });
-
-        number4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                text_plain.setText(text_plain.getText().toString() + "4");
-            }
-        });
-
-        number5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                text_plain.setText(text_plain.getText().toString() + "5");
-            }
-        });
-
-        number6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                text_plain.setText(text_plain.getText().toString() + "6");
-            }
-        });
-
-        number7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                text_plain.setText(text_plain.getText().toString() + "7");
-            }
-        });
-
-        number8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                text_plain.setText(text_plain.getText().toString() + "8");
-            }
-        });
-
-        number9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                text_plain.setText(text_plain.getText().toString() + "9");
-            }
-        });
-
-        equal.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                numbersecond = Float.parseFloat(text_plain.getText().toString());
-                switch (operator){
-                    case "+":
-                        float res = numberfirst + numbersecond;
-                        text_result.setText(String.valueOf(res));
-                        
-                }
-            }
-        });
-        
-
+        }
+        lastOperation = op;
+        operationField.setText(lastOperation);
     }
 
+    private void performOperation(Double number, String operation){
 
-
+        // если операнд ранее не был установлен (при вводе самой первой операции)
+        if(operand ==null){
+            operand = number;
+        }
+        else{
+            if(lastOperation.equals("=")){
+                lastOperation = operation;
+            }
+            switch(lastOperation){
+                case "=":
+                    operand =number;
+                    break;
+                case "/":
+                    if(number==0){
+                        operand =0.0;
+                    }
+                    else{
+                        operand /=number;
+                    }
+                    break;
+                case "*":
+                    operand *=number;
+                    break;
+                case "+":
+                    operand +=number;
+                    break;
+                case "-":
+                    operand -=number;
+                    break;
+            }
+        }
+        resultField.setText(operand.toString().replace('.', ','));
+        numberField.setText("");
+    }
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
