ead50fcf5426505de1766a30f2ef8d702cd34ab5
Fl1mas <eduard.fl1mas@gmail.com>
2024-03-15 22:13:22 +0800

    start 2 task

---
+++ app/src/main/java/com/example/vsgutulabs/LB4/RussianRoulette/RussianRouletteGame.java
@@ -0,0 +1,16 @@
+package com.example.vsgutulabs.LB4.RussianRoulette;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.vsgutulabs.R;
+
+public class RussianRouletteGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_russian_roulette_game);
+    }
+}
