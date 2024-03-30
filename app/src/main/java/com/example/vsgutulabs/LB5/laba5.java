804540c591afadefc06203c3e128dbd34ef841be
Fl1mas <eduard.fl1mas@gmail.com>
2024-03-30 22:34:38 +0800

    end 1task in 5lb

--- app/src/main/java/com/example/vsgutulabs/LB5/laba5.java
+++ app/src/main/java/com/example/vsgutulabs/LB5/laba5.java
@@ -1,5 +1,6 @@
 package com.example.vsgutulabs.LB5;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -14,14 +15,16 @@ import com.example.vsgutulabs.R;
 
 public class laba5 extends AppCompatActivity {
     private final static String TAG = "LIFECYCLE";
-
+    private static final String PRINT_TEXT_KEY = "PRINT_TEXT";
+    private static final int REQUEST_ACCESS_TYPE = 1;
     private TextView print;
     private EditText name;
     private EditText secondname;
     static final String NAME_KEY = "NAME";
     static final String NAMESECOND_KEY = "NAMESECOND";
     static final String ACCESS_MESSAGE = "ACCES";
-    private static final int REQUEST_ACCESS_TYPE = 1;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,31 +32,41 @@ public class laba5 extends AppCompatActivity {
         setContentView(R.layout.activity_laba5);
         Log.d(TAG, "activity_laba5 onCreate");
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            toolbar.setTitle("ВСГУТУ ЛАБЫ ПО МП"); // Title для приложения(надпись в самом верху)
+        }
+
         print = findViewById(R.id.print);
-
-    }
-
-    public void _btn_lb5_Auth(View view) {
         name = findViewById(R.id.name);
         secondname = findViewById(R.id.secondname);
 
+        if (savedInstanceState != null) {
+            name.setText(savedInstanceState.getString(NAME_KEY));
+            secondname.setText(savedInstanceState.getString(NAMESECOND_KEY));
+        }
+    }
+    public void _btn_lb5_Auth(View view) {
         String Name = name.getText().toString();
         String SecondName = secondname.getText().toString();
 
-        Intent intent = new Intent(this, laba5.class);
+        Intent intent = new Intent(this, lb5_auth_window.class);
         intent.putExtra(NAME_KEY, Name);
         intent.putExtra(NAMESECOND_KEY, SecondName);
         startActivityForResult(intent, REQUEST_ACCESS_TYPE);
+        Log.d(TAG, "activity_laba5 отправил данные во 2-ую активити");
 
 
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "activity_laba5 получил данные");
         if (requestCode == REQUEST_ACCESS_TYPE){
             if (resultCode == RESULT_OK){
                 String accessMessage = data.getStringExtra(ACCESS_MESSAGE);
                 print.setText(accessMessage);
+
             } else {
                 print.setText("ошибка доступа");
             }
@@ -62,29 +75,23 @@ public class laba5 extends AppCompatActivity {
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "activity_laba5 onSaveInstanceState called");
+        outState.putString(NAME_KEY, name.getText().toString());
+        outState.putString(NAMESECOND_KEY, secondname.getText().toString());
+        outState.putString(PRINT_TEXT_KEY, print.getText().toString());
+        Log.d(TAG, "activity_laba5 onSaveInstanceState finish");
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        if (toolbar != null) {
-            toolbar.setTitle("ВСГУТУ ЛАБЫ ПО МП"); // Title для приложения(надпись в самом верху)
-        }
     }
-
-
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d(TAG, "activity_laba5 onRestoreInstanceState called");
+        print.setText(savedInstanceState.getString(PRINT_TEXT_KEY));
+        name.setText(savedInstanceState.getString(NAME_KEY));
+        secondname.setText(savedInstanceState.getString(NAMESECOND_KEY));
+        Log.d(TAG, "activity_laba5 onRestoreInstanceState finish");
+    }
 }
--- app/src/main/java/com/example/vsgutulabs/LB5/lb5_auth_window.java
+++ app/src/main/java/com/example/vsgutulabs/LB5/lb5_auth_window.java
@@ -6,6 +6,8 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.vsgutulabs.R;
@@ -15,6 +17,7 @@ public class lb5_auth_window extends AppCompatActivity {
     TextView printname;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,9 +25,24 @@ public class lb5_auth_window extends AppCompatActivity {
         Log.d(TAG, "activity_lb5_auth_window onCreate");
 
         printname = findViewById(R.id.printname);
+        Button btn_1 = findViewById(R.id.btn_1);
+        Button btn_2 = findViewById(R.id.btn_2);
+        Button btn_3 = findViewById(R.id.btn_3);
+        Button btn_4 = findViewById(R.id.btn_4);
 
+        Bundle extras = getIntent().getExtras();
+        if (extras != null){
+            String name = extras.getString(laba5.NAME_KEY);
+            String secondname = extras.getString(laba5.NAMESECOND_KEY);
+            printname.setText(name + secondname);
+            btn_1.setText("Привет, " + name + " " + secondname);
+            btn_2.setText(name + " Ваша фамилия " + secondname);
+            btn_3.setText(name + secondname);
+            btn_4.setText("Отмена");
+
+        }
+        buttonInitialize();
     }
-
 
 
     @Override
@@ -36,4 +54,32 @@ public class lb5_auth_window extends AppCompatActivity {
             toolbar.setTitle("ВСГУТУ ЛАБЫ ПО МП"); // Title для приложения(надпись в самом верху)
         }
     }
+    private void buttonInitialize(){
+        Button btn_1 = findViewById(R.id.btn_1);
+        Button btn_2 = findViewById(R.id.btn_2);
+        Button btn_3 = findViewById(R.id.btn_3);
+        Button btn_4 = findViewById(R.id.btn_4);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message = ((Button) view).getText().toString();
+                Intent data = new Intent();
+                data.putExtra(laba5.ACCESS_MESSAGE, message);
+                setResult(RESULT_OK, data);
+                finish();
+            }
+        };
+        btn_1.setOnClickListener(listener);
+        btn_2.setOnClickListener(listener);
+        btn_3.setOnClickListener(listener);
+        btn_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setResult(RESULT_CANCELED);
+                finish();
+            }
+        });
+    }
+
+
 }
