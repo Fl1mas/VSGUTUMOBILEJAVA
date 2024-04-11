271607224d4e61a8628af995f468b31bf1b4fad7
Fl1mas <eduard.fl1mas@gmail.com>
2024-04-11 22:29:11 +0800

    start 2 task in 5lb

---
+++ app/src/main/java/com/example/vsgutulabs/LB5/NumberBigger/NumberBiggerOptions.java
@@ -0,0 +1,16 @@
+package com.example.vsgutulabs.LB5.NumberBigger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.vsgutulabs.R;
+
+public class NumberBiggerOptions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_number_bigger_options);
+    }
+}
